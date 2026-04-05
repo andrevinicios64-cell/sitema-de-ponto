c6cf3f27062711d6ef79d2b66ec055d9fd3c890e
Andre <andrevincios64@gmail.com>
2026-04-05 18:47:09 -0300

    fix erro servidor

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const pool = require('./db/database');
 
 app.use(express.json());
-app.use const path = require('path');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
